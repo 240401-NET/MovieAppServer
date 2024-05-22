49d091db24f746cb9a8ed8b3aa0834f10f8eb8ce
kwm0304 <106484883+kwm0304@users.noreply.github.com>
2024-05-22 14:50:26 -0400

    inheriting interface

--- server/Dto/FavoritedMovieDto.cs
+++ server/Dto/FavoritedMovieDto.cs
@@ -5,4 +5,6 @@ public class FavoritedMovieDto
   public string Username {get;set;}
   public string MovieTitle {get;set;}
   public int MovieId {get;set;} //since frontend will have access to Id, pass that as well
+  public string PosterPath {get;set;}
+  public string Description {get;set;}
 }
